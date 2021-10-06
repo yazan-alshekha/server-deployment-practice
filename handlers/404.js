50a44f3572303af7af725af3a0fa74361a539408
yazan alshekha <yazan.alshekha@outlook.com>
2021-10-06 22:21:28 +0300

    first lab

---
+++ handlers/404.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = (req, res, next) => {
+   
+    res.status(404).json({
+        code: 404,
+        route: req.path,
+        message: "Page Not Found"
+    });
+}
