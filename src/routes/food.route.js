090c036ea861572a753d3e3868c846c6ea812851
yazan alshekha <yazan.alshekha@outlook.com>
2022-03-03 16:48:43 +0300

    adding collection methodology

--- src/routes/food.route.js
+++ src/routes/food.route.js
@@ -20,14 +20,14 @@ function test(req,res){
 }
 // http://localhost:3030/food (get method)
 async function getFood(req, res) {
-    let record = await food.findAll();
+    let record = await food.readRecord();
     res.status(200).json(record)
 }
 // http://localhost:3030/food/2 (get method)
 async function getOneFood(req,res){
     let foodId =  req.params.id ;
     console.log('param',foodId);
-    let record =await food.findOne({where:{ id : foodId }});
+    let record =await food.readRecord({where:{ id : foodId }});
     res.status(200).json(record);
     
 }
@@ -36,21 +36,21 @@ async function getOneFood(req,res){
 async function createFood(req,res){
     let req_data = req.body;
     console.log('req bodyyy',req_data);
-    let record = await food.create(req_data);
+    let record = await food.createRecord(req_data);
     res.status(201).json(record);
 }
 
 // http://localhost:3030/food{"name":"omar"} (put method)
 async function editFood(req,res){
     let foodId =req.params.id;
-    let updateFood= await  food.update( req.body , {where:{id : foodId}} );
+    let updateFood= await  food.updateRecord( req.body , foodId );
     res.status(200).json(updateFood);
 }
 
 // http://localhost:3030/food/2 (delete method)
 async function deletefood(req,res){
     let foodId =  req.params.id;
-    let deletedFood=await food.destroy({where:{ id: foodId }});
+    let deletedFood=await food.deleteRecord( foodId );
     res.status(200).json(deletedFood);
 }
 
